f0ecf074a5ed79ebaebf948a88323ca34c8dd023
Ranjit Dhakal <dhakalranjit13@gmail.com>
2026-07-25 21:40:22 +0545

    Update and rename main.c to main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+
+int main() {
+    cout<<"\t\t\t***********************************************************************"<<endl;
+    cout<<"\t\t\t                                                                        "<<endl;
+	  cout<<"\t\t\t                       WELCOME TO Quiz Game                            "<<endl;
+    cout<<"\t\t\t                                                                        "<<endl;
+    cout<<"\t\t\t***********************************************************************"<<endl<<endl<<endl;
+
